7d6c5b0c3ad848e32e8f9b2625824f2316ee8460
harshit <harshitsaamu@gmail.com>
2018-05-25 02:50:09 +0530

    Notifications

---
+++ app/src/main/java/studentology/com/studentology/activity/notifications/NotificationActivity.kt
@@ -0,0 +1,13 @@
+package studentology.com.studentology.activity.notifications
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import studentology.com.studentology.R
+
+class NotificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification)
+    }
+}
