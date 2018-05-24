1dedddad27a5e2118ebf4fe32d741ccba3f46b2e
harshit <harshitsaamu@gmail.com>
2018-05-25 02:19:14 +0530

    HomeActivity Added

--- app/src/main/java/studentology/com/studentology/fragment/home/FragmentHome.kt
+++ app/src/main/java/studentology/com/studentology/fragment/home/FragmentHome.kt
@@ -5,7 +5,7 @@ import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import studentology.com.studentology.R
 
-class HomeActivity:AppCompatActivity() {
+class FragmentHome:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.fragment_home)
